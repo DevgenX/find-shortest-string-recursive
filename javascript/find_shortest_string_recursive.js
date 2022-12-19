3a04bfc1f4e53edaa675ebaf1b001e2a76280c17
devgenx <sebastian110420@gmail.com>
2022-12-19 08:43:05 -0500

    finished

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,21 +1,31 @@
 function findShortestStringRecursive(arr) {
   // type your code here
+  let shortest = arr[0];
+
+  if (arr.length <= 1) return arr[0];
+
+  if (arr[1].length < shortest.length) return arr[1];
+
+  return findShortestStringRecursive(arr.slice(1));
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestStringRecursive(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestStringRecursive(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+  console.log(
+    "=>",
+    findShortestStringRecursive(["flower", "juniper", "lily", "dandelion"])
+  );
 }
 
 module.exports = findShortestStringRecursive;
